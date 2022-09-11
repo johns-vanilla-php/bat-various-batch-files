d213b077169ff1c30167193fa6cb524f0e22b1b5
John's Vanilla PHP <112732248+johns-vanilla-php@users.noreply.github.com>
2022-09-11 10:49:43 -0400

    Update generate-github-keys.bat

--- ssh-keygen/generate-github-keys.bat
+++ ssh-keygen/generate-github-keys.bat
@@ -1,4 +1,3 @@
-::
 :: ssh-keygen.bat
 :: Version 1.0
 :: Created: 9/5/2022
@@ -11,11 +10,12 @@
 ::   Automatically updates the ~/.ssh/config file
 ::   Automatically updates the ~/.ssh/known_hosts file
 ::   Automatically saves the key information into the ~/.ssh/readme.txt file
-::
 
 @ECHO OFF
 TITLE Create SSH Key
 
+:: Define Screen Colors ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Setting color requires 2 digits...the first is the background color, and the other is text color
 : Colors:
 ::   00 = Black       08 = Gray
 ::   01 = Blue        09 = Light Blue
@@ -25,56 +25,32 @@ TITLE Create SSH Key
 ::   05 = Purple      0D = Light Purple
 ::   06 = Yellow      0E = Light Yellow
 ::   07 = White       0F = Bright White
-
 SET normColor=0B
 SET errColor=0C
 color %normColor%
 
+:: Github User Name ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :User-Prompt
 cls
 color %normColor%
 SET /P githubuser=Github User Name:
 IF not defined githubuser GOTO Msg1
 
+:: Define SSH File Name ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :File-Prompt
 cls
 color %normColor%
 SET /P filename=Filename:
 IF not defined filename GOTO Msg2
 
+:: Github Account (email) ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :Email-prompt
 cls
 color %normColor%
 SET /P email=Github Email:
 IF not defined email GOTO Msg3
 
-GOTO Keygen
-
-:Msg1
-cls
-SET gt=User-Prompt
-color %errColor%
-echo The Github User Name is REQUIRED
-GOTO ErrMsg
-
-:Msg2
-cls
-SET gt=File-Prompt
-color %errColor%
-echo The SSH Key Filename is REQUIRED
-GOTO ErrMsg
-
-:Msg3
-cls
-SET gt=Email-Prompt
-color %errColor%
-echo The Github email address is REQUIRED
-GOTO ErrMsg
-
-:ErrMsg
-pause
-GOTO %gt%
-
+:: Generate SSH Key ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :Keygen
 color %normColor%
 cls
@@ -82,17 +58,23 @@ echo Creating the SSH Keys
 echo.
 ssh-keygen -f %UserProfile%\.ssh\%filename% -t ed25519 -b 4096 -C "%email%"
 
+:: Prompt for Fingerprint ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :FingerPrint
 SET /P fingerprint=Copy the above key fingerprint and paste it here:
 clip < %UserProfile%\.ssh\%filename%.pub
 
-SET /P passphrase=Enter the passphrase that you used when creating the key:
+:: Prompt for Passphrase Used ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+SET /P passphrase=Enter the passphrase that you used when creating the key.  This can be blank:
 IF not defined passphrase GOTO EmptyPassphrase
 GOTO OpenURLs
 
+:: Set Passphrase to *blank* if it was left empty ::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Github Account (email) ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :EmptyPassphrase
 SET passphrase=*empty*
 
+:: Open Github URLs ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Github Account (email) ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :OpenURLs
 cls
 echo Two web pages will open in Firefox.
@@ -108,12 +90,18 @@ pause
 echo.
 start firefox.exe https://github.com/settings/ssh/new
 start firefox.exe https://github.com/settings/tokens/new
+
+:: Prompt for Personal Access Token ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 SET /P PAT=Paste the PAT from GitHub (https://github.com/settings/tokens/new) here:
 
+:: Open Github URLs ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Github Account (email) ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :SAVE
 cls
 cd %UserProfile%\.ssh\
 
+:: Open Github URLs ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Github Account (email) ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :readme-file
 echo. >> readme.txt
 echo github.com/%githubuser%>> readme.txt
@@ -125,6 +113,8 @@ echo   Access Token: %PAT%>> readme.txt
 echo Important information regarding this key has been saved to %UserProfile%\.ssh\readme.txt
 echo.
 
+:: Open Github URLs ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Github Account (email) ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :config-file
 echo. >> config
 echo. >> config
@@ -134,6 +124,8 @@ echo   IdentityFile ~/.ssh/%githubuser%>> config
 echo Your config file has been updated %UserProfile%\.ssh\config
 echo.
 
+:: Open Github URLs ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Github Account (email) ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 set /p Build=<%filename%.pub
 echo github.com %build%>> known_hosts
 echo Your known_hosts file has been updated %UserProfile%\.ssh\known_hosts
@@ -142,4 +134,43 @@ echo.
 echo Your new SSH keys have been installed!
 echo You may close this window.
 echo A backup of this information can be found in %UserProfile%\.ssh\readme.txt for future reference.
+pause
+GOTO exit
+
+:: Error Message - Missing User Name :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:Msg1
+cls
+SET gt=User-Prompt
+color %errColor%
+echo The Github User Name is REQUIRED
+GOTO ErrMsg
+
+:: Error Message - Missing SSH Key Filename ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:Msg2
+cls
+SET gt=File-Prompt
+color %errColor%
+echo The SSH Key Filename is REQUIRED
+GOTO ErrMsg
+
+:: Error Message - Missing Github Email Address ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:Msg3
+cls
+SET gt=Email-Prompt
+color %errColor%
+echo The Github email address is REQUIRED
+GOTO ErrMsg
+
+:: Error Message - Missing Github Email Address ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:ErrMsg
+pause
+GOTO %gt%
+
+:: Prompt to see if the user wants to create another key. ::::::::::::::::::::::::::::::::::::::::::::::
+: exit
+set /p another=Do you want to generate another key? ([y]es / [n]o)
+if another
+cls
+echo This script has completed and will now close.
 pause
